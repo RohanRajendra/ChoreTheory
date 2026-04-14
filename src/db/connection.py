aef871237076a645be8cfb6aac7010d7fc14eeff
RohanRajendra <rohan30raj@yahoo.co.in>
2026-04-13 22:01:50 -0400

    fix setup

--- src/db/connection.py
+++ src/db/connection.py
@@ -6,8 +6,9 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
+        host=os.getenv("DB_HOST", "localhost"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        database=os.getenv("DB_NAME", "project_db")
     )
+
